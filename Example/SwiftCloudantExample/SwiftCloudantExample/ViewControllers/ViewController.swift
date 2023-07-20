e1e1c536f79ac859b73191f0cec1f3b5bf47c28c
Dan Burkhardt <dan@gigabitelabs.com>
2023-07-20 11:05:16 -0400

    implemented list dbs and create new with crude uniqueness implementation, inactive buttons greyed out

--- Example/SwiftCloudantExample/SwiftCloudantExample/ViewControllers/ViewController.swift
+++ Example/SwiftCloudantExample/SwiftCloudantExample/ViewControllers/ViewController.swift
@@ -10,35 +10,132 @@ import SwiftCloudant
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBAction func getAllDBsButtonAction(_ sender: Any) {
+        getAllDBs()
+    }
+    @IBAction func createDBButtonAction(_ sender: Any) {
+        createDB()
+    }
     
     var couchClient: CouchDBClient?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        testLabel.text = "cats"
-        
-        // init swift cloudant FW
+        // UI config
+        setDefaultMessageLabel()
+        // FW config
         initSwifCloudant()
+    }
+    
+    func setDefaultMessageLabel() {
+        messageLabel.text = "(msg logs will print here)"
     }
     
     func initSwifCloudant() {
         
-        // if let
-        if let couchClient = couchClient {
-            
-        }
+        // init config
+        let config = ClientConfiguration(shouldBackOff: false)
         
-        // guard
+        // init local instance URL with no failure tolerance
         guard
-            let couchClient = couchClient
+            let couchURL = URL(string: "http://localhost:5984")
         else {
-            return
+            fatalError("test instance URL error")
         }
         
-        // optional invocation
-        // couchClient?.add(operation: )
+        // init client
+        couchClient = .init(url: couchURL,
+                            username: "admin",
+                            password: "p@ssw0rd")
+    }
+    
+    // utils
+    func updateMessagelabel(_ msg: String) {
+        DispatchQueue.main.async {
+            self.messageLabel.text = msg
+        }
+    }
+    
+    func incrementedDBNumber() -> Int {
+        // this is to crudely simulate randomness
+        // that each attempt to create a DB avoids
+        // colliding with any previous DB names without
+        // requiring manual edit of name string and app re-launch
+        //
+        //
+        // get last saved value for db name incrementation
+        let defaults = UserDefaults.standard
+        
+        // get and increment last DB number int
+        var dbNumber: Int = defaults.integer(forKey: "db-number-increment") ?? 0
+        dbNumber += 1
+        
+        // set incremented value
+        UserDefaults.standard.setValue(dbNumber, forKey: "db-number-increment")
+        
+        // return
+        return dbNumber
+    }
+    
+    // example operations
+    func getAllDBs() {
+        // create new operation
+        let allDbs = GetAllDatabasesOperation(
+           databaseHandler: { (databaseName) in
+           // do something for the database name
+        }) { (response, httpInfo, error) in
+            
+            // handle error
+           if let error = error {
+               let msg = error.localizedDescription
+               self.updateMessagelabel("error message: \(msg)")
+           }
+            
+            // handle unwrapped response
+            if let response = response {
+                // process the response information
+                var msgString = ""
+                 for dbNameRaw in response {
+                     if let dbName = dbNameRaw as? String {
+                         msgString += " \(dbName),"
+                     }
+                 }
+                // update message label
+                self.updateMessagelabel("all couchdb databases:\n\n \(msgString)")
+            }
+        }
+        // add to queue for execution
+        couchClient?.add(operation: allDbs)
+    }
+    
+    func createDB() {
+        // create new DB name string from DB increment
+        let newDBName = "client-created-db\(incrementedDBNumber())"
+        
+        // use DB name string in new db creation operation
+        let createDB = CreateDatabaseOperation(name: newDBName) { (response, info, error) in
+            
+            // handle error
+           if let error = error {
+               let msg = error.localizedDescription
+               self.updateMessagelabel("error message: \(msg)")
+           }
+            // handle unwrapped response
+            if let response = response {
+                if let error = response["error"] as? String {
+                    if let errorReason = response["reason"] as? String {
+                        self.updateMessagelabel("error occured:\n\n \(errorReason)")
+                    }
+                } else {
+                    if let successMsg = response["ok"] as? String {
+                        self.updateMessagelabel("success: database named \"\(newDBName)\" was created successfully")
+                    }
+                }
+            }
+        }
+        // add to queue for execution
+        couchClient?.add(operation: createDB)
     }
 }
 
